cd01e29e2f3d6dac2cc3d0d2cbc9b4c522c9a8fe
Daria Utkina <daria_utkina_ej@gmail.com>
2023-03-09 14:02:54 +0300

    add auth controller & fix readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-Article Blog:
+IT Articles Blog:
 --------------------
 
 It look like this for now (Test articles, test pictures)
 ![screen1.png](phantom.js%2Fscreen1.png)
+![img.png](img.png)
+![img_1.png](img_1.png)
--- models/User.php
+++ models/User.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "user".
@@ -16,7 +17,7 @@ use Yii;
  *
  * @property Comment[] $comments
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements IdentityInterface
 {
     /**
      * @inheritdoc
@@ -58,5 +59,65 @@ class User extends \yii\db\ActiveRecord
     public function getComments()
     {
         return $this->hasMany(Comment::className(), ['user_id' => 'id']);
+    }
+
+    public static function findIdentity($id)
+    {
+        return User::findOne($id);
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        // TODO: Implement getAuthKey() method.
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        // TODO: Implement validateAuthKey() method.
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        // TODO: Implement findIdentityByAccessToken() method.
+    }
+
+    public static function findByEmail($email)
+    {
+        return User::find()->where(['email'=>$email])->one();
+    }
+
+    public function validatePassword($password)
+    {
+        return ($this->password == $password) ? true : false;
+    }
+
+    public function create()
+    {
+        return $this->save(false);
+    }
+
+    public function saveFromVk($uid, $name, $photo)
+    {
+        $user = User::findOne($uid);
+        if($user)
+        {
+            return Yii::$app->user->login($user);
+        }
+
+        $this->id = $uid;
+        $this->name = $name;
+        $this->photo = $photo;
+        $this->create();
+
+        return Yii::$app->user->login($this);
+    }
+
+    public function getImage()
+    {
+        return $this->photo;
     }
 }
