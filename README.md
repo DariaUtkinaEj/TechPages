0183c303e33be0d68cf9cdb18cdf7caad5b66334
Daria Utkina <daria_utkina_ej@gmail.com>
2023-03-10 09:48:40 +0300

    hide admin module for non-admin users

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ IT Articles Blog:
 --------------------
 
 It look like this for now (Test articles, test pictures)
-![screen11.png](phantom.js%2Fscreen11.png)
 ![img_4.png](img_4.png)
+![screen11.png](phantom.js%2Fscreen11.png)
 ![img_2.png](img_2.png)
 ![img_3.png](img_3.png)
--- phantom.js/get_screen.js
+++ phantom.js/get_screen.js
@@ -3,10 +3,10 @@ page.viewportSize = {
     width: 1920,
     height: 1080
 };
-page.open('http://tech.loc/site/index?page=2&per-page=3', function() {
+page.open('http://tech.loc/', function() {
 
     setTimeout(function() {
-        page.render('screen111.png');
+        page.render('screen2.png');
         phantom.exit();
     }, 200);
 });
