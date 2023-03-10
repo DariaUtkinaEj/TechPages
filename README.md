41042ae50506c5929acdbae9a14b6647d938c4ab
Daria Utkina <daria_utkina_ej@gmail.com>
2023-03-10 08:40:33 +0300

    new readme

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ IT Articles Blog:
 
 It look like this for now (Test articles, test pictures)
 ![screen11.png](phantom.js%2Fscreen11.png)
-![img.png](img.png)
+![img_4.png](img_4.png)
 ![img_2.png](img_2.png)
 ![img_3.png](img_3.png)
--- phantom.js/get_screen.js
+++ phantom.js/get_screen.js
@@ -1,13 +1,12 @@
-// http://tech.loc/site/index?page=3&per-page=1
 var page = require('webpage').create();
 page.viewportSize = {
     width: 1920,
     height: 1080
 };
-page.open('http://tech.loc/', function() {
+page.open('http://tech.loc/site/index?page=2&per-page=3', function() {
 
     setTimeout(function() {
-        page.render('screen11.png');
+        page.render('screen111.png');
         phantom.exit();
     }, 200);
 });
