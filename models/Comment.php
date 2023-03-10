014d9c53ac5d0a5a58ac595fa126167fa2d56ba7
Daria Utkina <daria_utkina_ej@gmail.com>
2023-03-10 14:58:52 +0300

    add comment page into admin module, add CKeditor

--- models/Comment.php
+++ models/Comment.php
@@ -12,7 +12,6 @@ use Yii;
  * @property integer $user_id
  * @property integer $article_id
  * @property integer $status
- * @property string $date
  *
  * @property Article $article
  * @property User $user
@@ -22,6 +21,10 @@ class Comment extends \yii\db\ActiveRecord
     /**
      * @inheritdoc
      */
+
+    const STATUS_ALLOW = 1;
+    const STATUS_DISALLOW = 0;
+
     public static function tableName()
     {
         return 'comment';
@@ -34,7 +37,6 @@ class Comment extends \yii\db\ActiveRecord
     {
         return [
             [['user_id', 'article_id', 'status'], 'integer'],
-            [['date'], 'safe'],
             [['text'], 'string', 'max' => 255],
             [['article_id'], 'exist', 'skipOnError' => true, 'targetClass' => Article::className(), 'targetAttribute' => ['article_id' => 'id']],
             [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
@@ -52,7 +54,6 @@ class Comment extends \yii\db\ActiveRecord
             'user_id' => 'User ID',
             'article_id' => 'Article ID',
             'status' => 'Status',
-            'date' => 'Date',
         ];
     }
 
@@ -70,5 +71,27 @@ class Comment extends \yii\db\ActiveRecord
     public function getUser()
     {
         return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    public function getDate()
+    {
+        return Yii::$app->formatter->asDate($this->date);
+    }
+
+    public function isAllowed()
+    {
+        return $this->status;
+    }
+
+    public function allow()
+    {
+        $this->status = self::STATUS_ALLOW;
+        return $this->save(false);
+    }
+
+    public function disallow()
+    {
+        $this->status = self::STATUS_DISALLOW;
+        return $this->save(false);
     }
 }
