2e86f7995ce7517c4a1d5df63c0ed956a8b9db29
Daria Utkina <daria_utkina_ej@gmail.com>
2023-03-28 12:29:27 +0300

    fix sidebar recent posts

--- models/Article.php
+++ models/Article.php
@@ -176,7 +176,7 @@ class Article extends \yii\db\ActiveRecord
 
     public static function getRecent()
     {
-        return Article::find()->orderBy('date asc')->limit(4)->all();
+        return Article::find()->orderBy('date desc')->limit(4)->all();
     }
 
     public function getComments()
